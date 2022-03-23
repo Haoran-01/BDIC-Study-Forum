def48f6448fdd077add4b07cd770fd7c318bf226
阎浩然 <haoran.yan@ucdconnect.ie>
2022-03-23 18:39:34 +0800

    修改前后端连接问题

--- forms.py
+++ forms.py
@@ -1,10 +1,9 @@
 import wtforms
-from wtforms.validators import length,email,DataRequired,EqualTo
+from wtforms.validators import length,DataRequired,EqualTo
 
 class LoginFrom(wtforms.Form):
-    user_name = wtforms.StringField(validators=[DataRequired(), length(min=3,max=20)])
-    # email = wtforms.StringField(validators=[length(min=5,max=20),email()])
-    password = wtforms.StringField(validators=[DataRequired(), length(min=8,max=20)])
+    user_name = wtforms.StringField(validators=[length(min=3,max=20)])
+    password = wtforms.StringField(validators=[length(min=3,max=20)])
 
 class RegistForm(wtforms.Form):
     user_name = wtforms.StringField(validators=[DataRequired("用户名不能为空"), length(min=3, max=20)])
