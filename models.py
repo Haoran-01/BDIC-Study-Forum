1f64047384035e0d1f68fc40ab5b449101cfd189
吴文杰 <2334201198@qq.com>
2022-03-25 14:46:59 +0800

    添加邮箱发送功能

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from exts import db
-
+from datetime import datetime
 # 定义orm模型
 # autoincrement = True
 class User(db.Model):
@@ -7,3 +7,10 @@ class User(db.Model):
     user_email = db.Column(db.String(40), primary_key = True, nullable=False)
     user_name = db.Column(db.String(200), nullable = False)
     user_password = db.Column(db.Text, nullable = False)
+
+class EmailCaptchaModel(db.Model):
+    __tablename__ = "email_captcha"
+    id = db.Column(db.Integer, primary_key=True, autoincrement= True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    captcha = db.Column(db.String(10), nullable=False)
+    creat_time = db.Column(db.DateTime, default=datetime.now)
