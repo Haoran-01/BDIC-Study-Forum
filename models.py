f9976739a0d82084faa91f43ffa9671f7aa0ba96
吴文杰 <2334201198@qq.com>
2022-03-25 21:23:58 +0800

    完善邮件发送功能试之能存储至数据库

--- models.py
+++ models.py
@@ -4,14 +4,14 @@ from datetime import datetime
 # autoincrement = True
 
 class User(db.Model):
-    __tablename__ = 'user_TEST'
-    user_email = db.Column(db.String(200), primary_key = True, nullable=False, unique=True)
-    user_name = db.Column(db.String(200), nullable = False, unique=True)
-    user_password = db.Column(db.String(200), nullable = False, unique=True)
+    __tablename__ = 'user'
+    user_email = db.Column(db.CHAR(200), primary_key = True, nullable=False, unique=True)
+    user_name = db.Column(db.CHAR(200), nullable = False, unique=True)
+    user_password = db.Column(db.CHAR(200), nullable = False, unique=True)
 
 class EmailCaptchaModel(db.Model):
     __tablename__ = "email_captcha"
     id = db.Column(db.Integer, primary_key=True, autoincrement= True)
-    email = db.Column(db.String(100), nullable=False, unique=True)
-    captcha = db.Column(db.String(10), nullable=False)
+    email = db.Column(db.CHAR(100), nullable=False, unique=True)
+    captcha = db.Column(db.CHAR(10), nullable=False)
     creat_time = db.Column(db.DateTime, default=datetime.now)
