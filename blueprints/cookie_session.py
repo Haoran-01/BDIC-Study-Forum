add1e98125c96c200a06826db104e48f6e7d9332
Qi Sun <2769059069@qq.com>
2022-03-25 11:38:37 +0800

    更改项目结构

--- blueprints/cookie_session.py
+++ blueprints/cookie_session.py
@@ -1,36 +1,26 @@
-from flask import Flask,Response,request,session
+from flask import Flask,Response,request,session,Blueprint
 
-app = Flask(__name__)
-app.config['SECRET_KEY']='dskjfwqienkehyr1'
+bp = Blueprint("cookie&session", __name__, url_prefix="/cs")
 
-@app.route('/set_cookie')
+@bp.route('/set_cookie')
 def set_cookie():
     response=Response ("cookie 设置")
     response.set_cookie("user_id","XXX")
     return response
 
-@app.route('/get_cookie')
+@bp.route('/get_cookie')
 def get_cookie():
     user_id=request.cookies.get("user_id")
     print("user_id",user_id)
     return "获取cookie"
 
-@app.route('/set_session')
+@bp.route('/set_session')
 def set_session():
     session['username']='CyanPine'
     return "session 设置成功"
 
-@app.route('/get_session')
+@bp.route('/get_session')
 def get_session():
     username=session.get('username')
     print('username:',username)
     return "get_session"
-
-
-@app.route('/')
-def hello_world():
-    return 'hello world!'
-
-
-if __name__ == '__main__':
-    app.run()
