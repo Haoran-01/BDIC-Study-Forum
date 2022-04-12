d10d666c0315660b18e8f0cb7eca4b30c45c2e9b
吴文杰 <2334201198@qq.com>
2022-04-12 22:34:29 +0800

    添加和修改数据库的table

--- blueprints/forum.py
+++ blueprints/forum.py
@@ -1,30 +1,26 @@
 import json
 
 from flask import Blueprint, request, render_template, jsonify
+from models import QuestionType
 from forms import QuestionForm
 from exts import db
 
 bp = Blueprint("forum", __name__, url_prefix="/forum")
 
 @bp.route("/sections", methods=['GET', 'POST'])
-def forum_index():
-    if request.method == ["POST"]:
-        data = json.load(request.get_data(as_text=True))
-        return {"code": 200}
-    if request.method == ['GET']:
+def forum_index(data=None):
+    if request.method == "POST":
+        data = json.loads(request.get_data(as_text=True))
+        get_type_name = data["type_name"]
         if not get_type_name:
             return jsonify(msg="Request the type name")
-        type = QuestionForm.query.filter_by(type_name=get_type_name).first()
+        type = QuestionType.query.filter_by(type_name=get_type_name).first()
         if type:
-            title = type.type_name.data
-            total_post = type.total_post.data
-            today_post = type.today_post.data
-            today_comment = type.today_comment.data
-            rank = type.rank.data
-        return jsonify(title=title, total_post=total_post, today_post=today_post, today_comment=today_comment,rank=rank)
-
-
-
-
-
+            title = type.type_name
+            total_post = type.total_post
+            today_post = type.today_post
+            today_comment = type.today_comment
+            rank = type.rank
+        return jsonify(title=title, total_post=total_post, today_post=today_post, today_comment=today_comment,
+                       rank=rank)
 
