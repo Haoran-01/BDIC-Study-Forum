1aef39824b90fd52a67aa24fef722901f1da38e4
Qi Sun <2769059069@qq.com>
2022-04-12 22:44:53 +0800

    更新-不用直接使用（需要加域名或直接改成字符串）

--- blueprints/cookie_session.py
+++ blueprints/cookie_session.py
@@ -5,22 +5,33 @@ bp = Blueprint("Cookie_Session",__name__,url_prefix="/")
 @bp.route('/set_cookie/<name>')
 def set_cookie(name):
     response = Response ("cookie 设置")
-    response.set_cookie("user_email","yhr1019@163.com")
+    user_email=request.form.get();
+    user_id=request.form.get();
+    response.set_cookie("user_email",user_email)
+    response.set_cookie("user_id",user_id)
     return response
 
 @bp.route('/get_cookie')
 def get_cookie():
     user_id = request.cookies.get("user_id")
+    user_email = request.cookies.get("user_email")
     print("user_id", user_id)
     return "获取cookie"
 
 @bp.route('/set_session')
 def set_session():
-    session['username'] = 'CyanPine'
+    session['user_id'] = request.form.get();
+    session['user_email'] = request.form.get();
     return "session 设置成功"
 
 @bp.route('/get_session')
 def get_session():
-    username = session.get('username')
-    print('username:',username)
+    user_name = session.get('user_id')
+    user_email= session.get('user_email')
+
+    """
+    if (user_id == None | | user_email == null)
+        response = Response("请先登录")
+        return response
     return "get_session"
+    """
