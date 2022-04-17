eb205570cc670da29e974223aeb4f3920315d439
Qi Sun <2769059069@qq.com>
2022-04-17 18:12:06 +0800

    更新，加上userprofile

--- blueprints/__init__.py
+++ blueprints/__init__.py
@@ -2,3 +2,4 @@ from .user import bp as user_bp
 from .cookie_session import bp as cs_bp
 from .index import bp as index_bp
 from .forum import bp as forum_bp
+from .edit_profile import bp as userprofile_bp
