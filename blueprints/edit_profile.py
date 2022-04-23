979216f96904965552fd546ff64ccee253ceddb2
Qi Sun <2769059069@qq.com>
2022-04-23 23:25:01 +0800

    返回给网页部分格式修改

--- blueprints/edit_profile.py
+++ blueprints/edit_profile.py
@@ -3,6 +3,7 @@ from sqlalchemy.sql.functions import current_user
 from flask_login import login_required
 from models import UserProfile
 from exts import db
+import json
 
 bp = Blueprint("edit_profile",__name__,url_prefix="/")
 
@@ -26,5 +27,19 @@ def edit_profile():
 
     db.session.add(userprofile)
     db.session.commit()
-    return jsonify(code= 200, message= "save successfully!", data=data)
 
+    json_data = {
+        "user_name":data["user_name"],
+        "introduction":data["introduction"],
+        "user_email":data["user_email"],
+        "grade":data["grade"],
+        "department":data["department"],
+        "major":data["major"]
+    }
+
+    #print(json_data)
+    up_res_json = json.dumps(json_data)
+    #print(up_res_json)
+    return up_res_json
+
+    #return jsonify(code= 200, message= "save successfully!", data=data)
