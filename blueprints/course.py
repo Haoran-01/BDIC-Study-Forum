1e9704f3256718cbcd9a56c674b4e13b2e94b3e8
Qi Sun <2769059069@qq.com>
2022-04-25 23:59:28 +0800

    初步完成课表的五个接口

--- blueprints/course.py
+++ blueprints/course.py
@@ -22,8 +22,9 @@ def move_course():
 
     return {"success":200}
 
-@bp.login_required
-def course():
+@bp.route('/course/insert2',methods=['POST','GET'])
+@login_required
+def insert_course():
     data = request.get_json()
     course_id = Course.course_id
     front_id = data["course_id"]
@@ -34,16 +35,49 @@ def course():
     course_color = data["course_color"]
 
     course = Course(course_id,classroom,teacher,course_name,course_color)
-
-    db.session.query(Course).filter_by(Course.front_id ==front_id and Course.user_email==current_user_email).update(course)
+    sql = Course.front_id ==front_id and Course.user_email==current_user_email
+    db.session.query(Course).filter_by(sql).update(course)
     db.session.commit()
 
     return {"success":200}
 
+@bp.route('/course/delete',methods=['POST','GET'])
+def delete_course():
+    data = request.get_json()
+    front_id = data["course_id"]
+    current_user_email = current_user.user_email
+    sql = Course.front_id ==front_id and Course.user_email==current_user_email
+    res = db.session.query(Course).filter(sql).delete()
+    db.session.commit()
+    db.session.close()
+
+    return {"success":200}
+
+@bp.route('/course/query_single_course',methods=['POST','GET'])
+def query_single_course():
+    data = request.get_json()
+    front_id = data["course_id"]
+    current_user_email = current_user.user_email
+    sql = Course.front_id == front_id and Course.user_email == current_user_email
+    single_course = db.session.query(Course).filter(sql)
+    return jsonify({'classroom':single_course.classroom, 'teacher': single_course.teacher, 'course_name':single_course.course_name, 'course_color':single_course.course_color})
+
+@bp.route('/course/user_all_course',methods=['POST','GET'])
+def user_all_course():
+    user_all_course = db.session.query(Course).filter(Course.user_email == current_user.user_email).all()
+    result = []
+    for i in user_all_course:
+        dic = {
+            "course_id": i.front_id,
+            "x": i.x,
+            "y": i.y
+        }
+        result.append(dic)
+    return jsonify(comments=result)
+
 
 """
     my_id=str(course_id)
-
 
     json_data = {"items":{
         "course_id": my_id,
@@ -56,4 +90,5 @@ def course():
     #print(json_data)
     course_res_json = json.dumps(json_data)
     #print(course_res_json)
-    return course_res_json """
+    return course_res_json 
+    """
