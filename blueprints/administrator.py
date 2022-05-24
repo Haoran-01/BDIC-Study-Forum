3bea1baeb8da5a8a3d80234402f4b65a1c5a2bd8
Qi Sun <2769059069@qq.com>
2022-05-24 11:21:45 +0800

    总注册人数、总发帖数、总评论数

--- blueprints/administrator.py
+++ blueprints/administrator.py
@@ -1,7 +1,25 @@
+import flask_login
 from flask import Blueprint, request, jsonify
-from models import Administrator
+from models import Administrator, User, Comment, PostModel
 from exts import db
-from flask_login import login_required, current_user
 
 bp = Blueprint("administrator", __name__, url_prefix="/adm")
 
+@bp.route("/adm/registration", methods=['GET'])
+def get_registration_number():
+        # 总注册人数
+        registration_numbers = len(User.query.all())
+        return jsonify({'registration_numbers': registration_numbers}), 200
+
+
+@bp.route("/adm/gross_post", methods=['GET'])
+def get_gross_post():
+        # 总发帖数
+        gross_post_numbers = len(PostModel.query.filter_by().all())
+        return jsonify({'gross_post_numbers': gross_post_numbers}), 200
+
+@bp.route("/adm/gross_comment", methods=['GET'])
+def get_gross_post():
+        # 总评论数
+        gross_comment_numbers = len(Comment.query.filter_by().all())
+        return jsonify({'gross_post_numbers': gross_comment_numbers}), 200
